6cb17fe07ae4c0d914f3701c37b376c09e66838f
Saksha Shetty <shettysaksha4@gmail.com>
2025-09-06 17:51:20 +0530

    Add style and script

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submit handler
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault(); // Stops form from refreshing the page
+  alert("Thanks for your message!");
+});
